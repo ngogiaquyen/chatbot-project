d02f0ad908df0d080c49a725a9309085d2376803
quyenngogia <ngogiaquyendhtn223@gmail.com>
2025-05-15 09:04:22 +0700

    debug

--- chatbot_api/views.py
+++ chatbot_api/views.py
@@ -4,9 +4,6 @@ from django.http import JsonResponse
 import json
 import pymysql
 
-
-
-
 def test_sql():
     try:
         conn = pymysql.connect(
@@ -32,29 +29,31 @@ def test_sql():
 
 test_sql()
 
-# Tạo chatbot - KHÔNG train, chỉ sử dụng dữ liệu đã có
-chatbot = ChatBot(
-    "BotVN",
-    logic_adapters=["chatterbot.logic.BestMatch"],
-    read_only=True,  # RẤT QUAN TRỌNG: Ngăn không cho tự học lại
-    database_uri="mysql+pymysql://ukeptbsx_chat_bot:UkzkUpjzThj4cL4mS22V@103.97.126.29:3306/ukeptbsx_chat_bot?charset=utf8mb4"
-)
+chatbot = None
 
-# Xử lý API
 @csrf_exempt
 def chatbot_api(request):
+    global chatbot
+    if chatbot is None:
+        chatbot = ChatBot(
+            "BotVN",
+            logic_adapters=["chatterbot.logic.BestMatch"],
+            read_only=True,
+            database_uri="mysql+pymysql://ukeptbsx_chat_bot:UkzkUpjzThj4cL4mS22V@103.97.126.29:3306/ukeptbsx_chat_bot?charset=utf8mb4"
+        )
+
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
             user_message = data.get('message')
             if user_message:
-                # Lấy phản hồi từ chatbot
                 bot_response = chatbot.get_response(user_message)
                 return JsonResponse({'response': str(bot_response)})
             else:
                 return JsonResponse({'error': 'No message provided'}, status=400)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+
 
 @csrf_exempt
 def test_api(request):
