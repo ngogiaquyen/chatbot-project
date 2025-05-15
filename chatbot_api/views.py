434442fee8598e4f7c3e73bbb483e5c976faf7a7
quyenngogia <ngogiaquyendhtn223@gmail.com>
2025-05-15 15:57:01 +0700

    done

--- chatbot_api/views.py
+++ chatbot_api/views.py
@@ -7,8 +7,18 @@ from .models import find_best_answer
 def chatbot_api(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        user_question = data.get("message", "")
+        print(data)
+        user_question = data.get("content", "")
+        answer_data = data
+        answer_data["content"] = find_best_answer(user_question)["answer"]
 
-        answer_data = find_best_answer(user_question)
+        # Hoán đổi sender và receiver ID nếu cần
+        receiver_id = data.get("receiver_id")
+        sender_id = data.get("sender_id")
 
+        # Bạn có thể thêm vào answer_data nếu muốn phản hồi cả sender/receiver
+        answer_data["receiver_id"] = sender_id
+        answer_data["sender_id"] = receiver_id
+
+        print(answer_data)
         return JsonResponse(answer_data)
